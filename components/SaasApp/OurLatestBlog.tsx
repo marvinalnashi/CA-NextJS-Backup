c30a614e46906a8923d4afd24f3b709dd9e56176
Marvin Al Nashi <marvinalnashi@hotmail.com>
2024-03-04 16:06:50 +0100

    Update OurLatestBlog.tsx

--- components/SaasApp/OurLatestBlog.tsx
+++ components/SaasApp/OurLatestBlog.tsx
@@ -1,27 +1,6 @@
-import {GetStaticProps} from "next";
 import React from 'react'
 import { getAllPosts, getAllSettings, GhostSettings, GhostPostsOrPages } from '@lib/ghost'
-import {BodyClass} from "@helpers/BodyClass";
-import Link from 'next/link'
-import { Layout } from '@components/Layout'
-import { HeaderPage } from '@components/HeaderPage'
-import { PostCard } from '@components/PostCard'
-import { getLang, get } from '@utils/use-lang'
 import {HomePostCard} from "@components/HomePostCard";
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   // const posts = await getAllPosts({ limit: 3 })
-//   const posts = await getAllPosts({ limit: 3 })
-//   const settings = await getAllSettings()
-//
-//   return {
-//     props: {
-//       settings,
-//       posts,
-//       bodyClass: BodyClass({}),
-//     },
-//   }
-// }
 
 export interface HomeBlogProps {
   posts: GhostPostsOrPages
@@ -30,8 +9,6 @@ export interface HomeBlogProps {
 }
 
 const HomeBlog: React.FC<HomeBlogProps> = ({ posts, settings, bodyClass }) => {
-  const text = get(getLang(settings.lang));
-
   return (
       <div className="inner">
         <div className="post-feed">
